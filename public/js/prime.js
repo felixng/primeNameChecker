a9abfe7b9ff1ea6f649d428e90d9233f6cb06138
felix.ng <nfelix@gmail.com>
2016-04-27 08:08:35 +0100

    reactarise the form

--- public/js/prime.js
+++ public/js/prime.js
@@ -1,3 +1,77 @@
+// ****
+// Natural Language Form
+// ****
+var FreeTextBox = React.createClass({
+  componentDidMount: function() {
+    // this.inputsubmit.addEventListener( 'click', function( ev ) { ev.preventDefault(); self.close(); } );
+    // this.inputsubmit.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); self.close(); } );    
+  },
+  getInitialState: function() {
+    return { value: '', toggleClass: 'nl-field-toggle',  open: false};
+  },
+  handleChange: function(e) {
+    this.setState({value: e.target.value});
+  },
+  handleSubmit: function(e){
+    e.preventDefault(); e.stopPropagation(); 
+  },
+  handleToggle: function(e){
+    e.preventDefault(); e.stopPropagation(); 
+    if( this.state.open ) {
+      return false;
+    };
+    //this.setState{ open: true };
+    //this.form.fldOpen = this.pos;
+    //var self = this;
+    
+  },
+  render: function() {
+    return (
+      <div className="nl-field nl-ti-text">
+        <a className="nl-field-toggle " className={this.state.toggleClass} onClick={this.handleToggle}>{this.props.displayText}</a>
+        <ul>
+          <li className="nl-ti-input">
+            <input type="text" 
+                    placeholder={this.props.displayText} 
+                    value={this.state.name} 
+                    onCLick={this.handleSubmit}
+                    onChange={this.handleChange} />
+            <button className="nl-field-go">Go</button>
+          </li>
+          <li className="nl-ti-example">For example: <em>{this.props.exampleText}</em> //<em>Los Angeles</em> or <em>New York</em>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+});
+
+var NLForm = React.createClass({
+  render: function() {
+    return (
+      <form id="nl-form" className="nl-form">
+        My name is <FreeTextBox displayText='Christopher' exampleText='Christopher Boone'/>.
+        I live in <FreeTextBox displayText='somewhere in UK' exampleText='London, or New York'/>.
+        And you can reach me at <FreeTextBox displayText='detective@boone.com' exampleText='Christopher Boone'/>.
+        
+        <div className="nl-submit-wrap">
+          <button className="nl-submit" type="submit">Is my name a Prime Number?</button>
+        </div>
+        <div className="nl-overlay"></div>
+      </form>
+    );
+  }
+});
+
+ReactDOM.render(
+  <NLForm displayText='any city' exampleText='LA!'/>,
+  document.getElementById('main')
+);
+
+
+// ****
+// Single Question
+// ****
 var QuestionForm = React.createClass({
 	componentDidMount: function() {
         
